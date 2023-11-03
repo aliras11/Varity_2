3c06796537c256a27a2d8ab8fa730adc07c350f8
aliras11 <alirasoulzadehhoss@gmail.com>
2023-11-03 16:47:42 -0400

    added argument parsing

--- varity_run.py
+++ varity_run.py
@@ -8,16 +8,17 @@ import hp_tuning_skeleton as hp_tune
 from hyperopt import fmin, tpe, hp, STATUS_OK, Trials
 from pathlib import Path
 import datetime
+import argparse
 
-def main() -> int:
-    deleteme = 5556
+def main(config_path: str, tuning_rounds:str) -> int:
+    current_time = datetime.datetime.now()
     '''main function that executes a hyperparameter tuning session
         1. create and instantiate a varity dataloader instance 
         2. create a trials object for hyperopt to dump results into 
         3. pickle serialize trials object'''
     cp = Path.cwd()/Path('results_trials_folder')
     cp.mkdir(parents=True, exist_ok=True)
-    varity_data = data.Dataloader_Varity("/Users/alirezarasoulzadeh/Desktop/reimplemented_varity/test_config.json")
+    varity_data = data.Dataloader_Varity(config_path)
     trials = Trials()
     hp_dict = hp_tune.hp_space_builder_varity(varity_data.qip_dict)
     hp_dict.update({'varity_data':varity_data})
@@ -26,11 +27,15 @@ def main() -> int:
     best = fmin(hp_tune.core_targeted_CV,
     space = hp_dict,
     algo=tpe.suggest,
-    max_evals=10, trials=trials)
-    p = cp/Path(f"trial_object_hp_tuning_{deleteme}")
+    max_evals=int(tuning_rounds), trials=trials)
+    p = cp/Path(f"trial_object_hp_tuning_{current_time}")
     with p.open(mode="wb") as fp:
         pickle.dump(trials, fp)
 
     return 0
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("configuration_file_path", help="valid configuration JSON file")
+    parser.add_argument("number_tuning_round", help="Number of evaluations hyperopt should perform")
+    args = parser.parse_args()
+    main(args.configuration_file_path, args.number_tuning_round)
